8e0df1a47f91db571b1fb60f3ac67dfaa1c185b1
Jeff Hykin <jeff.hykin@gmail.com>
2023-02-07 15:18:36 -0600

    improve performance test error handling

--- programming_assignment_1/main/performance_test.py
+++ programming_assignment_1/main/performance_test.py
@@ -9,13 +9,17 @@ def run(cmd, timeout_sec):
     try:
         timer.start()
         stdout, stderr = proc.communicate()
-        if stderr:
-            print(f'''Error: {stderr.decode('utf-8')[0:-1]}''')
-        return stdout.decode('utf-8')[0:-1]
+        return stdout.decode('utf-8')[0:-1], stderr.decode('utf-8')[0:-1]
     finally:
         timer.cancel()
     return None
 
+def indent(string, by="    ", ignore_first=False):
+    indent_string = (" "*by) if isinstance(by, int) else by
+    string = string if isinstance(string, str) else f"{string}"
+    start = indent_string if not ignore_first else ""
+    return start + string.replace("\n", "\n"+indent_string)
+        
 # 
 # auto install ez_yaml
 # 
@@ -49,7 +53,7 @@ for base_test in base_tests:
         tests.append([ heuristic_name, *base_test ])
 
 def run_and_extract_data(heuristic, problem, layout):
-    output = run(" ".join(["python3", "pacman.py", "--timeout", "1", "--quiet_text_graphics", "-l", layout, "-p", "SearchAgent", "-a", f"prob={problem},heuristic={heuristic}", ]), timeout_sec=30)
+    output, error_output = run(" ".join(["python3", "pacman.py", "--timeout", "1", "--quiet_text_graphics", "-l", layout, "-p", "SearchAgent", "-a", f"prob={problem},heuristic={heuristic}", ]), timeout_sec=30)
     # seconds
     if not output:
         return [ None, None, None, "timed out" ]
@@ -69,9 +73,8 @@ def run_and_extract_data(heuristic, problem, layout):
     solution_length = None
     seconds = None
     try:
-        lines = output.split("\n")
-        lines = [ line for line in lines if not line.startswith("[SearchAgent]") ]
-        output = "\n".join(lines)
+        relevent_lines = output.split("\n")[-7:]
+        output = "\n".join(relevent_lines)
         output = output.replace(f"Path found with total cost of","Path found with total cost of:")
         output = output.replace(f"Warning: this does not look like a regular search maze\n","")
         data  = ez_yaml.to_object(
@@ -82,17 +85,34 @@ def run_and_extract_data(heuristic, problem, layout):
         solution_length, time = data['Path found with total cost of'].split(" in ")
         seconds, *_           = time.split(" ")
     except Exception as error:
-        import json
-        print(f'''error = {error}''')
-        print("")
-        print(f'''pacman_score = {pacman_score}''')
-        print(f'''nodes_expanded = {nodes_expanded}''')
-        print(f'''solution_length = {solution_length}''')
-        print(f'''seconds = {seconds}''')
-        print(f'''data = {dict(data)}''')
-        print("")
-        print(f'''output = {json.dumps(output)}''')
-        return [ None, None, None , None ]
+        import os
+        error_path = f"error/{heuristic}_{layout}.log"
+        os.makedirs(os.path.dirname(error_path), exist_ok=True)
+        with open(error_path, 'w') as the_file:
+            if error_output:
+                the_file.write(error_output)
+            else:
+                print("Tried to parse the output",                    file=the_file)
+                print("I expected some output that includes:",        file=the_file)
+                print("    Path found with total cost of",            file=the_file)
+                print("    Pacman emerges victorious! Score: ",       file=the_file)
+                print("    Search nodes expanded: ",                  file=the_file)
+                print("    Path found with total cost of: ",          file=the_file)
+                print("",                                             file=the_file)
+                print("however instead I got this output:",           file=the_file)
+                print(indent(output),                                 file=the_file)
+                print("",                                             file=the_file)
+                print("this is the error I got when parsing that:",   file=the_file)
+                print(indent(f"{error}"),                             file=the_file)
+                print("",                                             file=the_file)
+                print("this is partially parsed data:",               file=the_file)
+                print(f'''    pacman_score = {pacman_score}''',       file=the_file)
+                print(f'''    nodes_expanded = {nodes_expanded}''',   file=the_file)
+                print(f'''    solution_length = {solution_length}''', file=the_file)
+                print(f'''    seconds = {seconds}''',                 file=the_file)
+                print(f'''    data = {data}''',                       file=the_file)
+            
+        return [ None, None, None, error_path,  ]
     
     return pacman_score, nodes_expanded, int(solution_length), seconds
 
